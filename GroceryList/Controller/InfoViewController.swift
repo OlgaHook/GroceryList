99f82fccd1fc37dfba22b043def56814b4e5e362
OlgaHook <olga_krjuckova@inbox.lv>
2021-08-07 18:17:06 +0300

    ButtonsAddPopUpDeleteAllHalfReady

--- GroceryList/Controller/InfoViewController.swift
+++ GroceryList/Controller/InfoViewController.swift
@@ -9,17 +9,34 @@ import UIKit
 
 class InfoViewController: UIViewController {
     
-    @IBAction func infoVCCloseButtonTapped(_ sender: Any) {
+    @IBOutlet weak var openSettingsButton: UIButton!
+    
+  
+    @IBAction func InfoVCButtonTapped(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func openSettingsButtonTapped(_ sender: Any) {
+      openSettings()
+    }
+    func openSettings() {
+            guard let settingURL = URL(string: UIApplication.openSettingsURLString) else {
+                return
+            }
+            if UIApplication.shared.canOpenURL(settingURL){
+                
+                UIApplication.shared.open(settingURL, options: [:]) { success in
+                    print("success :", success)
+                }
+            }
+    }
+    
     /*
     // MARK: - Navigation
 
